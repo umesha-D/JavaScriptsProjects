852256481f5c17503185cacd3f9b495079b01d9b
umesha-D <umeshadsamanayaka@gmail.com>
2021-08-17 21:55:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let index = 0;
+
+function changeColors(){
+    let colors = ["red","blue","orange","yellow","green","purple"];
+
+    document.getElementsByTagName("body")[0].style.background = colors[index++];
+
+    if(index > colors.length -1)
+        index = 0;
+}
