b064c44adae5f380f65fc0b0990b64bed88a463a
umesha-D <umeshadsamanayaka@gmail.com>
2021-08-18 21:56:54 +0530

    script js updated

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let index = 0;
 function changeColors(){
     let colors = ["red","blue","orange","yellow","green","purple"];
 
-    document.getElementsByTagName("body")[0].style.background = colors[index++];
+    document.getElementsByTagName("body")[0].style.background = colors[index];
+	index = index + 1;
 
-    if(index > colors.length -1)
+    if(index > colors.length -1){
         index = 0;
+	}
 }
